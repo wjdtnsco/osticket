d69b117c79fc46d3c30c0641ea7c822e200445c6
wjdtnsco <72718775+wjdtnsco@users.noreply.github.com>
2020-10-13 16:33:46 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,9 +39,9 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','user211osticketsrv01.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
+define('DBUSER','azureadmin@user211osticketsrv01');
 define('DBPASS','demo@pass123');
 
 # Table prefix
